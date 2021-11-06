0d33354b6bf5189f4df225f962d6a1798aeb317d
Aman Verma <amanraoverma@gmail.com>
2021-11-06 16:34:24 -0400

    Remove go.sh dist.

--- go.sh
+++ go.sh
@@ -7,20 +7,6 @@ lint() {
     mypy source/ccombine.py
 }
 
-dist() {
-    local version="$(git describe --always --abbrev=16)"
-    if [[ -z $version ]]; then
-        return 1
-    fi
-    version="${version/#v/}"
-    local folder="ccombine-$version"
-    local tarball="$folder.tar"
-    local zstdcmd="zstd --rm --force -q -10 -T0"
-
-    git archive --verbose --prefix "${folder}/" --output "$tarball" @
-    $zstdcmd "$tarball"
-}
-
 help() { h; }
 h() {
     echo "$0 <task> [args]"
